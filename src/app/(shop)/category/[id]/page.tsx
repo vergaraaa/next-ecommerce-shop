a3abd7893db8270c78ca66d0f49a17327b97f0ad
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-24 13:34:07 +0200

    ✨ not found page on categories

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,16 @@
-export default function CartPage() {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function CartPage({ params }: Props) {
+  const { id } = await params;
+
+  if (id === "kids") return notFound();
+
   return (
     <div>
       <h1>Category Page</h1>
