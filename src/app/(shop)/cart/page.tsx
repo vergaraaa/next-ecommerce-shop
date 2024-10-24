92bef0eaa5726c547af46789d9c01adc1a2a7a75
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-24 18:59:17 +0200

    💄 cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,82 @@
+import { QuantitySelector } from "@/components/product/quantity-selector/QuantitySelector";
+import { Title } from "@/components/ui/title/Title";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function CartPage() {
   return (
-    <div>
-      <h1>Cart Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Cart" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 items-start">
+          {/* CART */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Add more items</span>
+
+            <Link href="/" className="underline mb-5">
+              Continue shopping
+            </Link>
+
+            {/* ITEMS */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{ height: 100, width: 100 }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
+
+                  <button className="underline">Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* CHECKOUT */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Order summary</h2>
+
+            <div className="grid grid-cols-2">
+              <span>Products</span>
+              <span className="text-right">3 items</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Taxes (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="text-2xl mt-5">Total</span>
+              <span className="text-right text-2xl mt-5">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <Link
+                href="/checkout/address"
+                className="flex btn-primary justify-center"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
